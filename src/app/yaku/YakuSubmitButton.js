2854ecb04002909ca45a00875307b951c18b2dde
i048iwai <kosuke5147@gmail.com>
2023-12-20 03:10:27 +0900

    子がツモのときの点数表記変更

--- src/app/yaku/YakuSubmitButton.js
+++ src/app/yaku/YakuSubmitButton.js
@@ -20,12 +20,16 @@ export default function YakuSubmitButton( props ){
 
         if( (oyaKo == '親') && (yakuman >= 1) ){
             calculateYakumanOya();
-        }else if( (oyaKo == '子') && (yakuman >= 1) ){
-            calculateYakumanKo();
-        }else if( (oyaKo == '親') && (isChiitoitsu) && (han <= 4) ){
+        }else if( (oyaKo == '子') && (yakuman >= 1) && (tsumoRon == 'ツモ')){
+            calculateYakumanTsumoKo();
+        }else if( (oyaKo == '子') && (yakuman >= 1) && (tsumoRon == 'ロン')){
+            calculateYakumanRonKo();
+        }else if( (oyaKo == '親') && (isChiitoitsu) && (han <= 4)){
             calculateChiitoitsuOya();
-        }else if( (oyaKo == '子') && (isChiitoitsu) && (han <= 4) ){
-            calculateChiitoitsuKo();
+        }else if( (oyaKo == '子') && (isChiitoitsu) && (han <= 4) && (tsumoRon == 'ツモ')){
+            calculateChiitoitsuTsumoKo();
+        }else if( (oyaKo == '子') && (isChiitoitsu) && (han <= 4) && (tsumoRon == 'ロン')){
+            calculateChiitoitsuRonKo();
         }else if( (oyaKo == '親') && (isPinfu) && (han <= 4) && (tsumoRon == 'ツモ')){
             calculatePinfuTsumoOya();
         }else if( (oyaKo == '親') && (isPinfu) && (han <= 4) && (tsumoRon == 'ロン')){
@@ -34,12 +38,12 @@ export default function YakuSubmitButton( props ){
             calculatePinfuTsumoKo();
         }else if( (oyaKo == '子') && (isPinfu) && (han <= 4) && (tsumoRon == 'ロン')){
             calculatePinfuRonKo();
-        }
-        else if( oyaKo == '親'){
+        }else if( oyaKo == '親'){
             calculateOver4hanOya(); 
-        }
-        else if( oyaKo == '子' ){
-            calculateOver4hanKo();
+        }else if( oyaKo == '子' && (tsumoRon == 'ツモ')){
+            calculateOver4hanTsumoKo();
+        }else if( oyaKo == '子' && (tsumoRon == 'ロン')){
+            calculateOver4hanRonKo();
         }
         
     }
@@ -63,7 +67,26 @@ export default function YakuSubmitButton( props ){
         }
     }
 
-    function calculateOver4hanKo(){
+    function calculateOver4hanTsumoKo(){
+        if(han <= 5){
+            setTotalPoint(2000+'・'+4000);
+            setNaniman('満貫');
+        }else if(han <= 7){
+            setTotalPoint(3000+'・'+6000);
+            setNaniman('跳満');
+        }else if(han <= 10){
+            setTotalPoint(4000+'・'+8000);
+            setNaniman('倍満');
+        }else if(han <= 12){
+            setTotalPoint(6000+'・'+12000);
+            setNaniman('三倍満');
+        }else if(han >= 13){
+            setTotalPoint(8000+'・'+16000);
+            setNaniman('数え役満');
+        }
+    }
+
+    function calculateOver4hanRonKo(){
         if(han <= 5){
             setTotalPoint(8000);
             setNaniman('満貫');
@@ -81,7 +104,7 @@ export default function YakuSubmitButton( props ){
             setNaniman('数え役満');
         }
     }
-    
+
     function calculateYakumanOya(){
         if(yakuman == 1){
             setTotalPoint(48000);
@@ -107,7 +130,32 @@ export default function YakuSubmitButton( props ){
         }
     }
 
-    function calculateYakumanKo(){
+    function calculateYakumanTsumoKo(){
+        if(yakuman == 1){
+            setTotalPoint(8000+'・'+16000);
+            setNaniman('役満');
+        }else if(yakuman == 2){
+            setTotalPoint(16000+'・'+32000);
+            setNaniman('二倍役満');
+        }else if(yakuman == 3){
+            setTotalPoint(24000+'・'+48000);
+            setNaniman('三倍役満');
+        }else if(yakuman == 4){
+            setTotalPoint(32000+'・'+64000);
+            setNaniman('四倍役満');
+        }else if(yakuman == 5){
+            setTotalPoint(40000+'・'+80000);
+            setNaniman('五倍役満');
+        }else if(yakuman == 6){
+            setTotalPoint(48000+'・'+96000);
+            setNaniman('六倍役満');
+        }else if(yakuman >= 7){
+            setTotalPoint(56000+'・'+112000);
+            setNaniman('七倍役満');
+        }
+    }
+
+    function calculateYakumanRonKo(){
         if(yakuman == 1){
             setTotalPoint(32000);
             setNaniman('役満');
@@ -142,7 +190,17 @@ export default function YakuSubmitButton( props ){
         }
     }
 
-    function calculateChiitoitsuKo(){
+    function calculateChiitoitsuTsumoKo(){
+        if(han == 2){
+            setTotalPoint(400+'・'+800);
+        }else if(han == 3){
+            setTotalPoint(800+'・'+1600);
+        }else if(han == 4){
+            setTotalPoint(1600+'・'+3200);
+        }
+    }
+
+    function calculateChiitoitsuRonKo(){
         if(han == 2){
             setTotalPoint(1600);
         }else if(han == 3){
@@ -176,11 +234,11 @@ export default function YakuSubmitButton( props ){
 
     function calculatePinfuTsumoKo(){
         if(han == 2){
-            setTotalPoint(1500);
+            setTotalPoint(400+'・'+700);
         }else if(han == 3){
-            setTotalPoint(2700);
+            setTotalPoint(700+'・'+1300);
         }else if(han == 4){
-            setTotalPoint(5200);
+            setTotalPoint(1300+'・'+2600);
         }
     }
 
