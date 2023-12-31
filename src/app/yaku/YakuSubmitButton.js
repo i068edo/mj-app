6171b1f96289e69227c010af616fe47c598adaea
i068edo <i520068z@mails.cc.ehime-u.ac.jp>
2023-12-31 14:17:36 +0900

    役満の点数計算のロジックを変更

--- src/app/yaku/YakuSubmitButton.js
+++ src/app/yaku/YakuSubmitButton.js
@@ -3,11 +3,6 @@ import { useState } from 'react';
 import Button from '@mui/material/Button';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import CalculateIcon from '@mui/icons-material/Calculate';
-import Box from '@mui/material/Box';
-import { amber } from '@mui/material/colors';
-
-import { useReducer } from 'react';
-
 
 export default function YakuSubmitButton( props ){
     const { setVisibleYakuField, setVisibleFuField, setVisibleResultField, 
@@ -45,6 +40,31 @@ export default function YakuSubmitButton( props ){
             setNaniman('数え役満');
         }
 
+        if( oyaKo=='親' ){
+            if( tsumoRon=='ツモ'){
+                setTotalPoint( basicPoint*2 );
+            }else{
+                setTotalPoint( basicPoint*6 );
+            }
+        }else{
+            if( tsumoRon=='ツモ'){
+                setTotalPoint( basicPoint + '・' + basicPoint*2 );
+            }else{
+                setTotalPoint( basicPoint*4 );
+            }
+        }
+    }
+
+    function calculateYakuman(){
+        basicPoint = 4000;
+        for ( let i = 0; i < yakuman; i++ ){
+            basicPoint *= 2;
+        }
+        if( yakuman == 1 ){
+            setNaniman('役満');
+        }else{
+            setNaniman( yakuman + '倍役満' );
+        }
         if( oyaKo=='親' ){
             if( tsumoRon=='ツモ'){
                 setTotalPoint( basicPoint*2 );
@@ -112,29 +132,12 @@ export default function YakuSubmitButton( props ){
 
     function calculateBranch(){
         if( yakuman > 0 ){
-            if( oyaKo == '親' ){
-                calculateYakumanOya();
-            }else if( tsumoRon == 'ロン' ){
-                calculateYakumanRonKo();
-            }else{
-                calculateYakumanTsumoKo();
-            }
+            calculateYakuman();
         }else if( han >= 5 ){
             calculatePointOverMangan();
         }else if( isChiitoitsu ){
             calculateChiitoitsu();
         }else if( isPinfu ){
-            /* if( oyaKo == '親' ){
-                if( tsumoRon == 'ロン'){
-                    calculatePinfuRonOya()
-                }else{
-                    calculatePinfuTsumoOya();
-                }
-            }else if( tsumoRon == 'ロン' ){
-                calculatePinfuRonKo();
-            }else{
-                calculatePinfuTsumoKo();
-            } */
             calculatePinfu();
         }else{
             initFu();
@@ -160,125 +163,6 @@ export default function YakuSubmitButton( props ){
         filedControleFromYaku();
         calculateBranch();
     }
-
-    function calculateYakumanOya(){
-        if( yakuman == 1 ){
-            setTotalPoint(48000);
-            setNaniman('役満');
-        }else if( yakuman == 2 ){
-            setTotalPoint(96000);
-            setNaniman('二倍役満');
-        }else if( yakuman == 3 ){
-            setTotalPoint(144000);
-            setNaniman('三倍役満');
-        }else if( yakuman == 4 ){
-            setTotalPoint(192000);
-            setNaniman('四倍役満');
-        }else if( yakuman == 5 ){
-            setTotalPoint(240000);
-            setNaniman('五倍役満');
-        }else if( yakuman == 6 ){
-            setTotalPoint(288000);
-            setNaniman('六倍役満');
-        }else if( yakuman >= 7 ){
-            setTotalPoint(336000);
-            setNaniman('七倍役満');
-        }
-    }
-
-    function calculateYakumanTsumoKo(){
-        if( yakuman == 1 ){
-            setTotalPoint(8000+'・'+16000);
-            setNaniman('役満');
-        }else if( yakuman == 2 ){
-            setTotalPoint(16000+'・'+32000);
-            setNaniman('二倍役満');
-        }else if( yakuman == 3 ){
-            setTotalPoint(24000+'・'+48000);
-            setNaniman('三倍役満');
-        }else if( yakuman == 4 ){
-            setTotalPoint(32000+'・'+64000);
-            setNaniman('四倍役満');
-        }else if( yakuman == 5 ){
-            setTotalPoint(40000+'・'+80000);
-            setNaniman('五倍役満');
-        }else if( yakuman == 6 ){
-            setTotalPoint(48000+'・'+96000);
-            setNaniman('六倍役満');
-        }else if( yakuman >= 7 ){
-            setTotalPoint(56000+'・'+112000);
-            setNaniman('七倍役満');
-        }
-    }
-
-    function calculateYakumanRonKo(){
-        if( yakuman == 1 ){
-            setTotalPoint(32000);
-            setNaniman('役満');
-        }else if( yakuman == 2 ){
-            setTotalPoint(64000);
-            setNaniman('二倍役満');
-        }else if( yakuman == 3 ){
-            setTotalPoint(96000);
-            setNaniman('三倍役満');
-        }else if( yakuman == 4 ){
-            setTotalPoint(128000);
-            setNaniman('四倍役満');
-        }else if( yakuman == 5 ){
-            setTotalPoint(160000);
-            setNaniman('五倍役満');
-        }else if( yakuman == 6 ){
-            setTotalPoint(192000);
-            setNaniman('六倍役満');
-        }else if( yakuman >= 7 ){
-            setTotalPoint(224000);
-            setNaniman('七倍役満');
-        }
-    }
-
-    /* function calculatePinfuTsumoOya(){
-        if( han == 2 ){
-            setTotalPoint(2100);
-        }else if( han == 3 ){
-            setTotalPoint(3900);
-        }else if( han == 4 ){
-            setTotalPoint(7800);
-        }
-    }
-
-    function calculatePinfuRonOya(){
-        if( han == 1 ){
-            setTotalPoint(1500); 
-        }else if( han == 2 ){
-            setTotalPoint(2900);
-        }else if( han == 3 ){
-            setTotalPoint(5800);
-        }else if( han == 4 ){
-            setTotalPoint(11600);
-        }
-    }
-
-    function calculatePinfuTsumoKo(){
-        if( han == 2 ){
-            setTotalPoint(400+'・'+700);
-        }else if( han == 3 ){
-            setTotalPoint(700+'・'+1300);
-        }else if( han == 4 ){
-            setTotalPoint(1300+'・'+2600);
-        }
-    }
-
-    function calculatePinfuRonKo(){
-        if( han == 1 ){
-            setTotalPoint(1000); 
-        }else if( han == 2 ){
-            setTotalPoint(2000);
-        }else if( han == 3 ){
-            setTotalPoint(3900);
-        }else if( han == 4 ){
-            setTotalPoint(7700);
-        }
-    } */
 
     function initFu(){
         setFu(20);
