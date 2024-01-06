c63d145b9f04c27f753c00d3ad1ff431e526539b
i068edo <i520068z@mails.cc.ehime-u.ac.jp>
2024-01-06 16:24:39 +0900

    アプリの説明の中身を実装

--- src/app/AppInfo.js
+++ src/app/AppInfo.js
@@ -3,12 +3,17 @@ import React from 'react';
 export default function AppInfo() {
     return (
         <div>
-            <span>アプリの説明です。</span>
-
-
-
-
-
+            <h3>このアプリの使い方</h3>
+            <div>
+                <ol type='1' >
+                    <li>ラジオグループは入力必須です。まずはこちらを入力しましょう。</li>
+                    <img src="photo/radioInfo.png" />
+                    <li>どの役で和了したのかを入力しましょう。</li>
+                    <img src="photo/yakuInfo.png" />
+                    <li>入力内容が正しければ「計算する」ボタンを押しましょう。</li>
+                    <img src="photo/buttonInfo.png" />
+                </ol>
+            </div>
         </div>
     );
 }
