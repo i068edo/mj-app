15fc6f5931377cef7c300375031d3b6facc68772
i068edo <i520068z@mails.cc.ehime-u.ac.jp>
2024-01-06 20:48:30 +0900

    favicon追加

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Head from 'next/head';
 /* import './globals.css' */
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <head>
+      <Head>
         <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -19,7 +20,7 @@ export default function RootLayout({ children }) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-      </head> */}
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
